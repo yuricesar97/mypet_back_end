1c1785af8f666b23d672f6de52894c4edf7d42be
Yuri Cesar <yuri.cesar-ext@ab-inbev.com>
2023-08-03 23:01:59 -0300

    ajuste mais

--- src/main/java/com/yuri/mypet/MyPetApplication.java
+++ src/main/java/com/yuri/mypet/MyPetApplication.java
@@ -1,24 +1,25 @@
 package com.yuri.mypet;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyPetApplication {
+public class MyPetApplication implements CommandLineRunner {
 
 
-// implements CommandLineRunner
+
 	public static void main(String[] args) {
 		SpringApplication.run(MyPetApplication.class, args);
 
 		
 	}
-//
-//	@Override
-//	public void run(String... args) throws Exception {
-//
-//
-//
-//	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+
+
+	}
 
 }
