34abc17b8afb85c469202a0a94a7629b2a2001ba
Yuri Cesar <yuri.cesar-ext@ab-inbev.com>
2023-08-03 22:57:37 -0300

    ajuste name pacote

--- src/main/java/com/yuri/mypet/MyPetApplication.java
+++ src/main/java/com/yuri/mypet/MyPetApplication.java
@@ -1,25 +1,24 @@
 package com.yuri.mypet;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyPetApplication implements CommandLineRunner {
+public class MyPetApplication {
 
 
-
+// implements CommandLineRunner
 	public static void main(String[] args) {
 		SpringApplication.run(MyPetApplication.class, args);
 
 		
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-	
-
-
-	}
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//
+//
+//
+//	}
 
 }
